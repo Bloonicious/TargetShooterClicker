d80836e315bc007646e532d4ed8492548ac8e98b
Bloonicious <155857894+Bloonicious@users.noreply.github.com>
2024-04-22 21:01:50 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 // Define variables for weapons, points, and upgrades
 let touchGunCost = 100;
 let touchGunPointsPerClick = 1;
+let touchGunLevel = 0;
+
 let pistolCost = 10;
 let pistolPointsPerShot = 1;
 let pistolFireRate = 1000; // in milliseconds

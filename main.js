48467d5f832fbd591947db32c68a1e324ee78604
Bloonicious <155857894+Bloonicious@users.noreply.github.com>
2024-04-27 23:50:25 +0100

    Update main.js

--- main.js
+++ main.js
@@ -848,6 +848,7 @@ function resetProgress() {
         localStorage.removeItem('gameState');
         // Reset all variables to their default values
         points = 0;
+        previousPoints = 0;
         // Reset achievements
         achievements.forEach(achievement => {
             achievement.achieved = false;

54df681a96d869a4ffa29174eb3b0e4f3ed89df4
Bloonicious <155857894+Bloonicious@users.noreply.github.com>
2024-05-03 21:51:17 +0100

    Update game.js

--- game.js
+++ game.js
@@ -3037,7 +3037,7 @@ updatePointsDisplay();
 updateCostDisplay();
 updateAchievementsDisplay();
 updateStatisticsDisplay();
-updateSelectedWeaponsDisplay();
+selectWeapon();
 
 // Start earning points automatically for purchased weapons
 setInterval(automaticPointsGeneration, 1000); // Check every second for points generation

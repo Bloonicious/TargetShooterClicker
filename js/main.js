f66e00677a6f71ead94ce0c22812d0189e9640df
Bloonicious <155857894+Bloonicious@users.noreply.github.com>
2024-05-24 21:24:45 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -841,6 +841,25 @@ function resetProgress() {
                 upgrades[weaponId][upgrade].bought = false;
             }
         }
+
+        // Reset touch gun big upgrades
+        upgrades.touchGun.pointyFingers.bought = false;
+        upgrades.touchGun.ambidextrous.bought = false;
+        upgrades.touchGun.thousandFingers.bought = false;
+        upgrades.touchGun.antirestingCream.bought = false;
+        upgrades.touchGun.powerfulHands.bought = false;
+        upgrades.touchGun.awakenUpgrade.bought = false;
+        upgrades.touchGun.millionFingers.bought = false;
+        upgrades.touchGun.stingingTaps.bought = false;
+        upgrades.touchGun.gotToTap.bought = false;
+        upgrades.touchGun.fingerSwarm.bought = false;
+        upgrades.touchGun.billionFingers.bought = false;
+        upgrades.touchGun.needMore.bought = false;
+        upgrades.touchGun.fingerPistols.bought = false;
+        upgrades.touchGun.superAwakenUpgrade.bought = false;
+        upgrades.touchGun.trillionFingers.bought = false;
+        upgrades.touchGun.heavyFingers.bought = false;
+        upgrades.touchGun.magicHands.bought = false;
         
         touchGunCost = 100;
         touchGunPointsPerClick = 1;
